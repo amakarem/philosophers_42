0e1deb10cdf2576236021dc8d29aebbb7db93569
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-01-03 16:09:50 +0100

    improve error msg

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 18:39:40 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/12/30 15:58:41 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/01/03 16:08:24 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ int	main(int argc, char **argv)
 
 	atexit(&leaks);
 	if (argc != 5 && argc != 6)
-		return (write_error("Wrong amount of arguments"));
+		return (write_error("usage: ./philo <number_of_philosophers> \
+<time_to_die> <time_to_eat> <time_to_sleep> \
+[number_of_times_each_philosopher_must_eat]"));
 	printf("\n starting... %s", argv[0]);
 	start_time = get_time();
 	usleep(10000);
