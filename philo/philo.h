0bd5061c0a2819548820e5c6417bf4cfc4e64221
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-10-14 14:53:50 +0200

    basic data structrure

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 13:35:03 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/10/14 13:53:41 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/10/14 14:53:25 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,56 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+typedef enum e_philo_state
+{
+	EATING = 0,
+	SLEEPING = 1,
+	THINKING = 2,
+	DEAD = 3,
+	FULL = 4,
+	IDLE = 5
+}	t_state;
+
+typedef struct s_philo
+{
+	int				id;
+	int				nb_meals_had;
+	struct s_data	*data;
+	t_state			state;
+	pthread_mutex_t	*left_f;
+	pthread_mutex_t	*right_f;
+	pthread_mutex_t	mut_state;
+	pthread_mutex_t	mut_nb_meals_had;
+	pthread_mutex_t	mut_last_eat_time;
+	uint64_t		last_eat_time;
+}	t_philo;
+
+typedef struct s_data
+{
+	int				philos_qty;
+	int				nb_meals;
+	int				nb_full_p;
+	bool			keep_loop;
+	uint64_t		eat_time;
+	uint64_t		die_time;
+	uint64_t		sleep_time;
+	uint64_t		start_time;
+	pthread_mutex_t	mut_eat_t;
+	pthread_mutex_t	mut_die_t;
+	pthread_mutex_t	mut_sleep_t;
+	pthread_mutex_t	mut_print;
+	pthread_mutex_t	mut_philos_qty;
+	pthread_mutex_t	mut_keep_loop;
+	pthread_mutex_t	mut_start_time;
+	pthread_t		monit_all_alive;
+	pthread_t		monit_all_full;
+	pthread_t		*philo_ths;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+}	t_data;
 
 int		validinput(int argc, char **argv);
 int		ft_atoi(const char *str);
