a35127a053417d3592487b95dd57fa9314ca5b10
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-10-14 18:43:29 +0200

    add main routine file

---
+++ philo/routine.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 18:05:20 by aelaaser          #+#    #+#             */
+/*   Updated: 2025/10/14 18:43:10 by aelaaser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	*routine(void *philo_p)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *) philo_p;
+	mutex_update_u64(&philo->mut_last_eat_time, &philo->last_eat_time, get_time());
+	if (philo->id % 2 == 0)
+		ft_usleep(philo->data->eat_time - 10);
+	while (mutex_get_int(&philo->mut_state, &philo->state) != DEAD)
+	{
+		if (eat(philo) != 0)
+			break ;
+		if (mutex_get_int(&philo->mut_state, &philo->state) == DEAD)
+			break ;
+		if (to_sleep(philo) != 0)
+			break ;
+		if (mutex_get_int(&philo->mut_state, &philo->state) == DEAD)
+			break ;
+		if (think(philo) != 0)
+			break ;
+	}
+	return (NULL);
+}
+
+static void	kill_all_philos(t_data *data)
+{
+	t_philo	*philos;
+	int		i;
+	int		philos_qty;
+
+	philos_qty = get_philos_qty(data);
+	philos = data->philos;
+	i = -1;
+	while (++i < philos_qty)
+		set_philo_state(&philos[i], DEAD);
+}
+
+static void	*all_full_routine(void *data_p)
+{
+	t_data	*data;
+	int		i;
+	int		philos_qty;
+
+	data = (t_data *)data_p;
+	i = -1;
+	philos_qty = get_philos_qty(data);
+	while (++i < philos_qty && get_keep_loop(data))
+	{
+		usleep(1);
+		if (get_nb_meals_philo_had(&data->philos[i]) < data->nb_meals)
+			i = -1;
+	}
+	if (get_keep_loop(data) == true)
+	{
+		mutex_update_int(&data->mut_keep_loop, &data->keep_loop, 0);
+		kill_all_philos(data);
+	}
+	return (NULL);
+}
+
+
+static void	*all_alive_routine(void *data_p)
+{
+	int		i;
+	int		philos_qty;
+	t_data	*data;
+	t_philo	*philos;
+
+	data = (t_data *)data_p;
+	philos = data->philos;
+	philos_qty = get_philos_qty(data);
+	i = -1;
+	while (++i < philos_qty && get_keep_loop(data))
+	{
+		if (is_died(&philos[i]) && get_keep_loop(data))
+		{
+			mutex_print(data, philos[i].id, MSG_DIED);
+			mutex_update_int(&data->mut_keep_loop, &data->keep_loop, 0);
+			kill_all_philos(data);
+			break ;
+		}
+		if (i == philos_qty - 1)
+			i = -1;
+		usleep(1000);
+	}
+	return (NULL);
+}
+
+int	create_threads(t_data *data)
+{
+	int	i;
+	int	nb_of_philos;
+
+	nb_of_philos = mutex_get_int(&data->mut_philos_qty, &data->philos_qty);
+	i = -1;
+	data->start_time = get_time();
+	while (++i < nb_of_philos)
+	{
+		if (pthread_create(&data->philo_ths[i], NULL,
+				&routine, &data->philos[i]))
+			return (1);
+	}
+	if (pthread_create(&data->monit_all_alive, NULL,
+			&all_alive_routine, data))
+		return (1);
+	if (data->nb_meals > 0
+		&& pthread_create(&data->monit_all_full, NULL,
+			&all_full_routine, data))
+		return (1);
+	return (0);
+}
